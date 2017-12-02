edcef9116a1a51cf9da574ab0df2f34cffd42c0c
flomko <flomko.ru@gmail.com>
2017-12-02 22:08:24 +0300

    worked simple test

--- day1/captcha.py
+++ day1/captcha.py
@@ -1,5 +1,12 @@
 
 def output():
+    """Just read and return capcha"""
     with open('input.txt', 'r') as data:
        intext = data.readline().strip()
     return(int(intext))
+
+def work(chain):
+    sum = 0
+    if chain[0] == chain[-1]:
+        sum += int(chain[0])
+    return sum
--- day1/utest_captch.py
+++ day1/utest_captch.py
@@ -2,8 +2,16 @@ import captcha
 import unittest
 
 class CapTest(unittest.TestCase):
-    def test_return(self):
+    def test_output_return(self):
+        """output should be int"""
+        print ( "id: " + self .id())
         self.assertIsInstance(captcha.output(), int)
+    
+    def test_case(self):
+        """correctly work with input"""
+        print ( "id: " + self .id())
+        self.assertEqual(captcha.work('11'), 1)
+
 
 if __name__ == '__main__' :
     unittest.main()
