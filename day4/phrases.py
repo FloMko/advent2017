db0c717eb10b5e4df8f33947286e2eede162321a
flomko <flomko.ru@gmail.com>
2017-12-05 02:02:59 +0300

    succes

--- day4/phrases.py
+++ day4/phrases.py
@@ -3,3 +3,10 @@ def input():
     with open('input.txt', 'r') as data:
         phrases = [line.split() for line in data.readlines()]
     return phrases
+
+def main():
+    phrases = input()
+    return sum(len(phrase) == len(set(phrase)) for phrase in phrases)
+
+if __name__ == '__main__':
+    main()
