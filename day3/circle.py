d29e97f8c800d66206c0d47be38f67c9748cf923
flomko <flomko.ru@gmail.com>
2017-12-05 01:28:07 +0300

    find solution to move from Frojdholm

--- day3/circle.py
+++ day3/circle.py
@@ -12,5 +12,38 @@ def set_node(matrix, i=0, j=0, offset=0):
     matrix[i+offset][j+offset] = neighbors(matrix, 0, 0, 0)
     return matrix
 
+def findway():
+    size = 60
+    matrix = [[0 for _ in range(size)] for _ in range(size)]
+    offset = size//2 + 1
+    num = 1
+    target = 289326
+    matrix[offset][offset] = num
+
+
+    i = 0
+    j = 0
+    step_x = True
+    step = 1
+    n = 1
+    step_size = 1
+
+    while num <= target:
+        if step_x:
+            i += step
+        else:
+            j += step
+        n -= 1
+        if n == 0:
+            if not step_x:
+                step = -step
+                step_size += 1
+
+            step_x = not step_x
+            n = step_size
+        num = neighbors(matrix, i, j, offset)
+        matrix[i+offset][j+offset] = num
+    return num
+
 if __name__ == '__main__':
     findway()
