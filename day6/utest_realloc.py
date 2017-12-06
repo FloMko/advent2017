27bd482dfe5978c321f0583bcde9193b5522ca90
flomko <flomko.ru@gmail.com>
2017-12-06 10:16:25 +0300

    add test find max

--- day6/utest_realloc.py
+++ day6/utest_realloc.py
@@ -6,10 +6,14 @@ class CapTest(unittest.TestCase):
         """store input"""
         print ( "id: " + self.id())
         self.assertIsInstance(realloc.indata(), list)
-    def test_case(self):
+    def test_case_final(self):
+        """correctly find max index"""
+        print ( "id: " + self.id())
+        self.assertEqual(realloc.maxfind([0,2,7,7]), 2) 
+    def test_case_final(self):
         """correctly work with input"""
         print ( "id: " + self.id())
-        self.assertEqual(realloc.turn([0,2,7,0]), 5)
+        self.assertEqual(realloc.main([0,2,7,0]), 5)
 
 if __name__ == '__main__' :
     unittest.main()
