854906c85337d4f82b5e9ddd297ee7e802f29ac4
flomko <flomko.ru@gmail.com>
2017-12-08 09:34:02 +0300

    add test for chunking record

--- day7/utest_tower.py
+++ day7/utest_tower.py
@@ -6,6 +6,11 @@ class CapTest(unittest.TestCase):
         """store input"""
         print ( "id: " + self.id())
         self.assertIsInstance(tower.indata(), list)
+    def test_indata_chunk(self):
+        """detect loop"""
+        print ( "id: " + self.id())
+        self.assertEqual(tower.chunk('ehsqyyb (174) -> xtcdt, tujcuy, wiqohmb, cxdwmu'
+            ),[ehsqyyb, 174, True])
 
 if __name__ == '__main__' :
     unittest.main()
