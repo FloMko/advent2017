8cf0e7c0bc7e90ce00bdcf63350d6e2d0259ac03
flomko <flomko.ru@gmail.com>
2017-12-09 16:16:21 +0300

    pep8 style

--- day6/realloc.py
+++ day6/realloc.py
@@ -5,21 +5,25 @@ def indata():
         instr = [int(bank) for bank in instr]
     return instr
 
+
 def maxfind(intext):
     return max(intext), intext.index(max(intext))
+
 
 def detect(intext, aknowleged):
     if intext in aknowleged:
         return False
 
+
 def aknowleg(intext, aknowleged=[]):
     aknowleged.append(intext)
     return aknowleged
 
+
 def distribute(intext, i, n):
     """redistrib memory
-       take state memory bank, index val ,max value 
-       return modified memory bank, 
+    take state memory bank, index val ,max value 
+    return modified memory bank, 
     """
     z = i
     while n > 0:
@@ -29,11 +33,12 @@ def distribute(intext, i, n):
     intext[z] = 0
     return intext
 
+
 def stepcount(memory, not_find_yet=True):
     aknowleged = []
     step = 0
     while not_find_yet:
-        not_find_yet = detect(memory,aknowleged)
+        not_find_yet = detect(memory, aknowleged)
         aknowleged = aknowleg(memory, aknowleged)
         val, pos = maxfind(memory)
         memory = distribute(memory, pos, val)
@@ -47,12 +52,12 @@ def main(not_find_yet=True):
     step = 0
     while not_find_yet:
         aknowleged = aknowleg(memory, aknowleged)
-        not_find_yet = detect(memory,aknowleged)
+        not_find_yet = detect(memory, aknowleged)
         val, pos = maxfind(memory)
         memory = distribute(memory, pos, val)
         step += 1
     return step
-    
+
+
 if __name__ == '__main__':
     main()
-
