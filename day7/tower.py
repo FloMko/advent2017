1e6c00ede6bed3fb2ccde7c5031cfca380804ed4
flomko <flomko@localhost.localdomain>
2017-12-10 16:19:25 +0300

    formaat pep8

--- day7/tower.py
+++ day7/tower.py
@@ -4,6 +4,7 @@ def indata():
         instr = [line.strip() for line in data]
     return instr
 
+
 def chunk(instr):
     """prepare record to analyze
        return name, posithion and state
@@ -20,30 +21,35 @@ def chunk(instr):
     else:
         return name, leaf, None, weight
 
+
 def aknowleg(leaflist, aknowleged=set()):
     for prog in leaflist:
         aknowleged.add(prog)
     return(aknowleged)
 
+
 def aknowleg_prog(programm, aknowleged_prog=set()):
     aknowleged_prog.add(programm)
     return(aknowleged_prog)
 
-def buld_tower(inlist = indata()):
+
+def buld_tower(inlist=indata()):
     tower = {}
     for line in inlist:
         name, leaf, leap, weight = chunk(line)
         tower[name] = weight, leaf, leap
     return tower
 
+
 def balance(tower, name):
     root_weight = tower[name][1]
     leafs = tower[name][2]
     weight = 0
-    if leafs != None:
+    if leafs is not None:
         for leaf in leafs:
             weight += tower[leaf][1]
     return root_weight >= weight
+
 
 def root():
     aknowleged = set()
@@ -51,12 +57,13 @@ def root():
     tower = buld_tower()
     for name in tower.keys():
         leap = tower[name][2]
-        if leap != None:
+        if leap is not None:
             aknowleged = aknowleg(leap, aknowleged)
         aknowleged_prog = aknowleg_prog(name, aknowleged_prog)
     for prog in aknowleged_prog:
         if prog not in aknowleged:
             return(prog)
+
 
 def unbalance():
     aknowleged = set()
@@ -64,15 +71,7 @@ def unbalance():
     tower = buld_tower()
     for name in tower.keys():
         unbalance = balance(tower, name)
-        if unbalance != True:
-            leap = tower[name][2]    
-            if leap != None:
-                aknowleged = aknowleg(leap, aknowleged)
-            aknowleged_prog = aknowleg_prog(name, aknowleged_prog)
-    for prog in aknowleged_prog:
-        if prog not in aknowleged:
-            print(prog)
+
 
 if __name__ == '__main__':
     unbalance()
-
