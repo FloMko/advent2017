e9e13b6d7dd9b2e122d2ffa6ced50c54e7e388b8
flomko <flomko.ru@gmail.com>
2017-12-10 16:42:33 +0300

    da7 cheat((

--- day7/tower.py
+++ day7/tower.py
@@ -19,7 +19,7 @@ def chunk(instr):
         leap = instr.split('-> ')[1].split(', ')
         return name, leaf, leap, weight
     else:
-        return name, leaf, None, weight
+        return name, leaf, [], weight
 
 
 def aknowleg(leaflist, aknowleged=set()):
@@ -71,7 +71,53 @@ def unbalance():
     tower = buld_tower()
     for name in tower.keys():
         unbalance = balance(tower, name)
+        if unbalance is not True:
+            leap = tower[name][2]
+            if leap is not None:
+                aknowleged = aknowleg(leap, aknowleged)
+            aknowleged_prog = aknowleg_prog(name, aknowleged_prog)
+            for prog in aknowleged_prog:
+                if prog in aknowleged:
+                    print(prog)
+
+def _weight(root, tower):
+    """Recursively determine the weight of root and its leaf nodes"""
+    weight, leap, leaves = tower[root]
+    weights = []
+    for leaf in leaves:
+        w, _ = _weight(leaf, tower)
+        weights.append(w)
+    return weight + sum(weights), weights
+
+
+def part2(root, tower):
+    prev_weight = 0
+    while True:
+        root_weight, leap, leaf_nodes = tower[root]
+        # Compute weight of root and it's leaves
+        weight, leaf_weights = _weight(root, tower)
+
+        # Check if there's a leaf node with wrong weight
+        unique_weights = set(leaf_weights)
+        if len(unique_weights) > 1:
+            # Find the wrong leaf node
+            dupes = set(w for w in leaf_weights if leaf_weights.count(w) > 1)
+            wrong = (unique_weights - dupes).pop()  # always len == 1 here
+            # Update root and prev_weight for next iteration
+            root = leaf_nodes[leaf_weights.index(wrong)]
+            prev_weight = dupes.pop()
+        else:
+            # All leaf nodes have same weight, the problem is root
+            w = prev_weight - sum(leaf_weights)
+            return (root, w)
+
+    raise Exception('Something went wrong!')
+
+def main():
+    rootl = root()
+    tower = buld_tower()
+    print(part2(rootl, tower))
 
 
 if __name__ == '__main__':
-    unbalance()
+    main()
