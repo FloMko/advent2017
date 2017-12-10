63035edca867dfa781dd51faddc036546ab2cd52
flomko <flomko.ru@gmail.com>
2017-12-10 19:36:38 +0300

    init test garbage sstore and rewrite

---
+++ day9/utest_garbage.py
@@ -0,0 +1,18 @@
+import garbage
+import unittest
+
+
+class CapTest(unittest.TestCase):
+    def test_input_store(self):
+        """store input"""
+        print ("id: " + self.id())
+        self.assertIsInstance(garbage.indata(), str)
+
+    def test_rewrite_input(self):
+        """chain input to store"""
+        print ("id: " + self.id())
+        self.assertEqual(garbage.rewrite('<!>>'), '<>')
+
+
+if __name__ == '__main__':
+    unittest.main()
