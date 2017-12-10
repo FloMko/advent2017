72c2ae3c1b681a51c170ac9e8d743bbbca380258
flomko <flomko.ru@gmail.com>
2017-12-10 19:41:51 +0300

    pass input test

---
+++ day9/garbage.py
@@ -0,0 +1,11 @@
+
+def indata():
+    """Just read and store"""
+    with open('input.txt', 'r') as data:
+        instr = data.readline()
+    return instr
+
+
+
+if __name__ == '__main__':
+    main()
