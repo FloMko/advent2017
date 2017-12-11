ded77410f3a7501105437797d723d585a204975e
flomko <flomko.ru@gmail.com>
2017-12-11 08:58:14 +0300

    add test for reverse

--- day10/utest_knot.py
+++ day10/utest_knot.py
@@ -8,6 +8,15 @@ class CapTest(unittest.TestCase):
         print ("id: " + self.id())
         self.assertIsInstance(knot.indata(), list)
 
+    def test_reverse_section(self):
+        """
+        get circular list, position in list and lenght of reverse
+        return modified list
+        """
+        print ("id: " + self.id())
+        self.assertEqual(knot.reverse([0, 1, 2, 3, 4], 0, 3), [2,1,0,3,4])
+
+
 
 if __name__ == '__main__':
     unittest.main()
