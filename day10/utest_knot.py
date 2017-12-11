6b2df8911049beed2db18768cf1dd617d88e9873
flomko <flomko.ru@gmail.com>
2017-12-11 09:14:23 +0300

    add list test for out of range

--- day10/utest_knot.py
+++ day10/utest_knot.py
@@ -15,6 +15,14 @@ class CapTest(unittest.TestCase):
         """
         print ("id: " + self.id())
         self.assertEqual(knot.reverse([0, 1, 2, 3, 4], 0, 3), [2,1,0,3,4])
+    
+    def test_reverse_section_out_of_list(self):
+        """
+        get circular list, position in list and lenght of reverse
+        return modified list
+        """
+        print ("id: " + self.id())
+        self.assertEqual(knot.reverse([2,1,0,3,4], 3, 4), [4,3,0,1,2])
 
 
 
