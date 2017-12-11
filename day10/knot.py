23fd633772e2f89c8c01934456d701bd6ad49edf
flomko <flomko.ru@gmail.com>
2017-12-11 09:10:13 +0300

    passed test with reverse

--- day10/knot.py
+++ day10/knot.py
@@ -5,6 +5,16 @@ def indata():
         instr = data.readline().split()
     return instr
 
+def reverse(inlist, pos, length):
+        """
+        get circular list, position in list and lenght of reverse
+        return modified list
+        """
+        rever = inlist[pos:length]
+        rever.reverse()
+        for position in range(pos, pos +  length):
+            inlist[position] = rever[position - length]
+        return inlist
 
 if __name__ == '__main__':
     main()
